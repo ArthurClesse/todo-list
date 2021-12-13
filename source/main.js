00f8cb70a3aee6f1c959a8e03403b84eee5d8427
arthur <arthur@apart.lu>
2021-12-13 17:59:51 +0100

    Start project, landing, fan cards, page message

--- source/main.js
+++ source/main.js
@@ -1,5 +1,43 @@
 import "./styles.scss";
 
+var elem = document.querySelector('.main-carousel');
+// options
+asNavFor: '.main-carousel';
+// set as a selector string
+
+asNavFor: document.querySelector('.main-carousel');
+
+
+var flkty = new Flickity( elem, {
+    // options
+    contain: true,
+    prevNextButtons: false,
+    wrapAround: true,
+    imagesLoaded: true,
+    watchCSS: true
+});
+
+var flktyNav = new Flickity( ".carousel-nav", {
+    // options
+    asNavFor: '.main-carousel',
+    prevNextButtons: false,
+    pageDots: false,
+    watchCSS: true
+});
+
+var cards = document.querySelectorAll('.carousel-cell');
+
+cards.forEach(function (card, index) {
+    card.addEventListener("mouseenter", function (e){
+
+        document.getElementById(e.target.dataset.card).classList.add("show");
+    });
+    card.addEventListener("mouseleave", function (e){
+
+        document.getElementById(e.target.dataset.card).classList.remove("show");
+    });
+});
+
 class App {
 
 }
